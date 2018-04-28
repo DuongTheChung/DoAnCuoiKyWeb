e8fe0af802608c70bbe1a1a75c585539fc05cbdf
Chung Duong <duongchung666@gmail.com>
2018-04-28 11:46:50 +0700

    Create structure project,Router simple

--- app.js
+++ app.js
@@ -1,60 +1,31 @@
-var http=require('http');
+var express=require('express');
 var path=require('path');
-var fs=require('fs');
-var url=require('url');
 
-const port = process.argv[2] || 8080;
-http.createServer(function (req, res) {
-  console.log(`${req.method} ${req.url}`);
-  // parse URL
-  const parsedUrl = url.parse(req.url);
-  // extract URL path
-  let pathname = `.${parsedUrl.pathname}`;
-  // maps file extention to MIME types
-  const mimeType = {
-    '.ico': 'image/x-icon',
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.json': 'application/json',
-    '.css': 'text/css',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.wav': 'audio/wav',
-    '.mp3': 'audio/mpeg',
-    '.svg': 'image/svg+xml',
-    '.pdf': 'application/pdf',
-    '.doc': 'application/msword',
-    '.eot': 'appliaction/vnd.ms-fontobject',
-    '.ttf': 'aplication/font-sfnt',
-    '.svg': 'image/svg+xml',
-    '.woff2':'application/font-woff2',
-    '.woff':'application/font-woff'
+//init app
+var app=express();
+
+//set path views
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
 
 
-  };
-  fs.exists(pathname, function (exist) {
-    if(!exist) {
-      res.statusCode = 404;
-      res.end(`File ${pathname} not found!`);
-      return;
-    }
+//set public client folder
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public/admin')));
 
-    if (fs.statSync(pathname).isDirectory()) {
-      pathname += '/index.html';
-    }
-    // read file from file system
-    fs.readFile(pathname, function(err, data){
-      if(err){
-        res.statusCode = 500;
-        res.end(`Error getting the file: ${err}.`);
-      } else {
-        // based on the URL path, extract the file extention. e.g. .js, .doc, ...
-        const ext = path.parse(pathname).ext;
-        // if the file is found, set Content-type and send data
-        res.setHeader('Content-type', mimeType[ext] || 'text/plain' );
-        res.end(data);
-      }
-    });
-  });
-}).listen(parseInt(port));
-console.log(`Server listening on port ${port}`);
+
+var homeClient=require('./routes/client/index');
+var homeAdmin=require('./routes/admin/index');
+app.use('/admin',homeAdmin);
+app.use('/',homeClient);
+
+//set server
+
+var port=3000;
+app.listen(port,()=>{
+  console.log('Server started on port '+ port);
+});
+
+
+
+
---
+++ routes/admin/index.js
@@ -0,0 +1,10 @@
+var express=require('express');
+var router=express.Router();
+
+router.get('/',(req,res)=>{
+    res.render('admin/index',{
+        title:'Home admin'
+    });
+});
+
+module.exports=router;
