81738ce045e92b2755793eb3f4a5577c291d0d7d
Chung Duong <duongchung666@gmail.com>
2018-04-29 21:16:42 +0700

    edit model

--- models/categories.js
+++ models/categories.js
@@ -1,17 +1,34 @@
 var db=require('../config/DBConnection');
 
+
+//Categoty
 var Category={
 
-    getAllCategory:function(callback){
-        return db.query("select * from category",callback);
+    getAllCategory : function(category,callback) {
+        return db.query("select * from category Limit ?,?",[category.offset,category.limit],callback);
+    },
+
+    categoryCount : function(callback) {
+       return db.query("select count(1) as count from category", callback);
     },
 
     getCategoryById:function(id,callback){
         return db.query("select * from category where id=?",[id],callback);
     },
 
+    findCategoryByName:function(name,callback){
+        return db.query("select* from category where name=?",[name],callback);
+    },
+
+    findCategoryByMetatitle:function(meta_title,id,callback){
+        return db.query("select * from category where meta_title=? and id != ?",[meta_title,id],callback);
+    },
+
     addCategory:function(category,callback){
-        return db.query("Insert into category(name) values(?)",[category.name],callback);
+        return db.query("Insert into category(name,meta_title,display_order,"
+            +"created_date,created_by) values(?,?,?,?,?)",
+        [category.name,category.meta_title,category.display_order
+        ,category.created_date,category.created_by],callback);
     },
 
     deleteCategory:function(id,callback){
@@ -19,7 +36,10 @@ var Category={
     },
 
     updateCategory:function(id,category,callback){
-        return db.query("update category set nam=? where id=?",[category.name,id],callback);
+        return db.query("update category set name=?,meta_title=?,"
+        +"display_order=?,modified_date=?,modified_by=? where id=?",
+        [category.name,category.meta_title,category.display_order,category.modified_date,
+         category.modified_by,id],callback);
     }
 }
 
